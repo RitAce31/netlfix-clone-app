49234216d965c3e20a9fd904bb027e82de9d2a08
RitAce <ritace31@gmail.com>
2023-10-24 12:13:38 +0530

    admin cofigure

--- api/index.js
+++ api/index.js
@@ -29,6 +29,7 @@ app.use(
       "http://localhost:3000",
       "http://127.0.0.1:3000",
       "https://ritace.netlify.app",
+      "https://ritaceadmin.netlify.app",
       "http://localhost:4000",
       "http://127.0.0.1:4000",
     ],
