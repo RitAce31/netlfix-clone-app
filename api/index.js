131571eee4572ebe7aee800e23203cbac553f3dd
jindalmistry <mistryjindal2@gmail.com>
2023-11-03 18:44:24 +0530

    Url Update

--- api/index.js
+++ api/index.js
@@ -29,6 +29,7 @@ app.use(
       "http://localhost:3000",
       "http://127.0.0.1:3000",
       "https://portfolioflix.netlify.app",
+      "https://ritace.netlify.app",
       "http://localhost:4000",
       "http://127.0.0.1:4000",
     ],
