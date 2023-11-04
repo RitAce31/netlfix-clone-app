1082549e8a28a85499e3e9f42f0d25155e6472a1
jindalmistry <mistryjindal2@gmail.com>
2023-11-04 15:26:47 +0530

    vercel url for admin dashboard

--- api/index.js
+++ api/index.js
@@ -28,9 +28,8 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://127.0.0.1:3000",
-      "https://portfolioflix.netlify.app",
-      "https://ritace.netlify.app",
       "https://portfolioflix.vercel.app",
+      "https://portfolioadminflix.vercel.app",
       "http://localhost:4000",
       "http://127.0.0.1:4000",
     ],
