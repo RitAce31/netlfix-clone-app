e6467e1d6d7854a5d6959c27fa8a7032c2a68af6
Ritace <ritace31@gmail.com>
2023-11-04 15:49:14 +0530

    update

--- admin/src/services/Services.js
+++ admin/src/services/Services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://netflix-clone-app-gfn8.onrender.co";
+const url = "https://netflix-clone-app-gfn8.onrender.com";
 
 //LOGIN API
 export const loginUser = (email, password) => {
